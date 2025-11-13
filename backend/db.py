eddec0c7077020eda735fdb21806c9062338256f
jhonatanT04 <venotacu@gmail.com>
2025-11-13 08:05:25 -0500

    Se debe tener una base de datos local con las credenciales del archivo db.py

--- backend/db.py
+++ backend/db.py
@@ -2,22 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import URL
 
-# url = URL.create(
-#     drivername= "postgresql",
-#     username= "postgres",
-#     password="root",
-#     host="localhost",
-#     database= "db",
-#     port=5432
-# )
 url = URL.create(
     drivername= "postgresql",
-    username= "admin",
-    password="admin1234",
+    username= "postgres",
+    password="root",
     host="localhost",
     database= "db",
-    port = 4040
+    port=5432
 )
+# url = URL.create(
+#     drivername= "postgresql",
+#     username= "admin",
+#     password="admin1234",
+#     host="localhost",
+#     database= "db",
+#     port = 4040
+# )
 
 engine = create_engine(url=url, echo=True)
 
