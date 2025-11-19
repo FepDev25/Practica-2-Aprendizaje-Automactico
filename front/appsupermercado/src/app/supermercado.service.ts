ae3671f05e79845dd1dad23e87e07764c0a78f08
JustinLucero07 <justinluceroxd04@gmail.com>
2025-11-18 22:47:33 -0500

    conexion con el backend y estilos

--- front/appsupermercado/src/app/supermercado.service.ts
+++ front/appsupermercado/src/app/supermercado.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,28 @@ import { Injectable } from '@angular/core';
 })
 export class SupermercadoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  apiUrl = 'http://34.10.83.87:8000';
+  getPedriccion(fecha: string, nombre: string) {
+    return this.http.get<any[]>(
+      `${this.apiUrl}/predictPornombre/`,
+      { params: { fecha, nombre } }
+    );
+  }
+
+  getReportePorFecha(fecha: string) {
+    return this.http.get<any[]>(
+      `${this.apiUrl}/predictAll/`,
+      { params: { fecha } }
+    );
+  }
+
+  subirCsv(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post("TU_URL/upload_csv", formData);
+  }
+
 }
--- front/appsupermercado/src/app/supermercado/supermercado.component.ts
+++ front/appsupermercado/src/app/supermercado/supermercado.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupermercadoService } from '../supermercado.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-supermercado',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './supermercado.component.html',
-  styleUrl: './supermercado.component.scss'
+  styleUrls: ['./supermercado.component.scss']
 })
-export class SupermercadoComponent {
+export class SupermercadoComponent implements OnInit {
+  prediccionResultado: any = null;
+  mensajeResumen: string = '';
+  fecha: string = '';
+  nombre: string = '';
+  fechaReporte: string = '';
+  reporte: any[] = [];
+
+  constructor(private service: SupermercadoService) { }
+
+  ngOnInit(): void { }
+
+  getPedict(fecha: string, nombre: string) {
+    if (!fecha || !nombre) {
+      alert('Debe ingresar el producto y la fecha');
+      return;
+    }
+
+    this.service.getPedriccion(fecha, nombre).subscribe(data => {
+      console.log('Predicción recibida:', data);
+      this.prediccionResultado = data;
+    });
+  }
+
+  generarReporte() {
+    this.service.getReportePorFecha(this.fechaReporte).subscribe(data => {
+      console.log('Reporte recibido:', data);
+
+      const resp: any = data;
+      this.reporte = resp.predictions ?? [];
+      this.mensajeResumen = resp.mensaje_resumen;
+    });
+  }
+
+  subirCsv() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.csv';
+
+    input.onchange = () => {
+      const file = input.files?.[0];
+      if (file) {
+        this.service.subirCsv(file).subscribe(resp => {
+          console.log('CSV subido:', resp);
+          alert('CSV subido correctamente');
+        });
+      }
+    };
+
+    input.click();
+  }
 
 }
